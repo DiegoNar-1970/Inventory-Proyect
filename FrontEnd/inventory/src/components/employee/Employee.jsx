d53ce102d236ff7188b1d80398b720f1871c4c73
DiegoNar-1970 <diegonaranjo303@gmail.com>
2024-07-04 23:15:43 -0500

    update layout

--- FrontEnd/inventory/src/components/employee/Employee.jsx
+++ FrontEnd/inventory/src/components/employee/Employee.jsx
@@ -2,7 +2,7 @@
 import { IoPersonAddOutline } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
 // import { FaHelmetSafety } from "react-icons/fa6";
-
+import img from '../../media/img/img.png'
 const Employee = () => {
   return (
     <div className="flex flex-col flex-wrap flex-1 rounded-lg gap-3 text-white">
@@ -14,11 +14,13 @@ const Employee = () => {
             <span className="text-[14px] font-thin">control de empleados</span>
           </div>
         </div> */}
-        <div className="flex gap-2 items-center bg-gray-800 rounded-2xl ">
-          <input className="rounded-2xl border-[1px] outline-none border-black p-[6px]
+        <div className="flex gap-2 items-center bg-gray-500 rounded-2xl ">
+          <input className="rounded-2xl outline-none  p-[6px]
           rounded-r-none text-black" 
           placeholder="nombre" type="search" />
-          <IoSearch className="text-[30px] text-white min-w-[30px] h-full mr-[10px]"/>
+          <button className=" text-white min-w-[30px] h-full mr-[10px]">
+            <IoSearch className="text-[30px]"/>
+          </button>
         </div>
         <div className="flex items-center gap-2">
           <IoPersonAddOutline className="text-[30px] text-green-500" />
@@ -28,19 +30,27 @@ const Employee = () => {
         </div>
       </article>
       <article className="flex flex-col bg-fondo-menu rounded-lg box-border p-2 justify-center">
-              <h1 className="text-red-500">hols</h1>
+              <h1 className=" ml-1 text-[17px] text-gray-500">Personal</h1>
           <table className="text-left text-[13px] border-collapse text-white rounded-r-lg">
             <thead className="text-white rounded-r-lg ">
               <tr className="" >
-                <th className="border-b-[1px] border-gray-500">Cedula</th>
                 <th className="border-b-[1px] border-gray-500">nombre</th>
                 <th className="border-b-[1px] border-gray-500">apellido</th>
+                <th className="border-b-[1px] border-gray-500">Cedula</th>
                 <th className="border-b-[1px] border-gray-500">area</th>
+                <th className="border-b-[1px] border-gray-500">Estado</th>
                 <th className="border-b-[1px] border-gray-500">Acciones</th>
               </tr>
             </thead>
               <tr>
-                <td>100</td> <td>diego</td> <td>narajo</td> <td>almacen</td> 
+                <td className="flex gap-2 items-center">
+                  <img className="w-[25px] h-full rounded-[2em]"src={img} alt="" />
+                  <span>Diego</span>
+                </td>
+                 <td className="text-gray-500">narajo</td> 
+                <td >100</td> 
+                 <td className="text-gray-500">almacen</td> 
+                <td>💚activo 🖤</td> 
                 <td>
                   <button className="bg-green-500 text-white rounded-[1em] p-[2px]">Actualizar</button>
                 </td>
