bd4fe60c52e4293150c0b91db5e1afec67dcc338
DiegoNar-1970 <diegonaranjo303@gmail.com>
2024-07-16 21:20:56 -0500

    add Search

--- FrontEnd/inventory/src/components/employee/Employee.jsx
+++ FrontEnd/inventory/src/components/employee/Employee.jsx
@@ -2,10 +2,11 @@
 
 // import { FaHelmetSafety } from "react-icons/fa6";
 import Table from "../smallComponents/Table.jsx";
-import Search from "../smallComponents/Search.jsx";
 import { useParams } from "react-router-dom";
 import  {fetchData} from '../../helpers/fetchData.js'
 import { useEffect,useState } from "react";
+import { IoPersonAddOutline } from "react-icons/io5";
+import { IoSearch } from "react-icons/io5";
 
 
 const apiData=fetchData('http://localhost:3000/employee');
@@ -13,36 +14,42 @@ const Employee = () => {
 
   let {area}=useParams();
   const data= apiData.read();
-  const [filterData,setFilterData]=useState();
+  const [areaFilter,setAreaFilter]=useState();
+  const [nameFilter,setNameFilter]=useState();
 
   useEffect(()=>{
-    try{
-      let dataFilter=data.filter(employee=>employee.area===area);
-      setFilterData(dataFilter);
-    }catch(err){
-      return(
-        <span>Not Found</span>
-      )
-    }
-    
-  },[area])
+    let dataFilter=data.filter(employee=>employee.area===area);
+    setAreaFilter(dataFilter);
+  },[area,data])
   
+
+  const searchFilter=({target})=>{
+    target.value 
+    ? setNameFilter(areaFilter.filter(employee=>employee.profile.name.toLowerCase().includes(target.value.toLowerCase())))
+    :setNameFilter(areaFilter)
+    
+  }
   return (
     <div className="flex flex-col flex-wrap flex-1 rounded-lg gap-3 text-white">
       <article className="flex gap-2 justify-around items-center bg-fondo-menu rounded-lg p-2 box-border  ">
-          <Search/>
+       <div className="flex gap-2 items-center bg-gray-500 rounded-2xl ">
+          <input onChange={searchFilter} className="rounded-2xl outline-none  p-[6px]
+          rounded-r-none text-black" 
+          placeholder="nombre" type="search" />
+
+            <IoSearch className="text-[30px]"/>
+        </div>
+        <div className="flex items-center gap-2">
+          <IoPersonAddOutline className="text-[30px] text-green-500" />
+          <button>agregar persona</button>
+        </div>
+        <div>
+        </div>
       </article>
       <article className="flex flex-col bg-fondo-menu rounded-lg box-border p-2 justify-center">
-          <Table data={filterData}/>
+          <Table data={nameFilter ? nameFilter : areaFilter}/>
       </article>
     </div>
   )
 }
 export default Employee
-        {/* <div className="flex gap-2 items-center text-center ">
-          <FaHelmetSafety className="text-[40px] text-center text-orange-300"/>
-          <div className="flex flex-col">
-            <h1 className="text-4xl text-green-500">Empleados</h1>
-            <span className="text-[14px] font-thin">control de empleados</span>
-          </div>
-        </div> */}
